e0003eaa6017eb7b6f303acf608c9c460c3908ec
Lavashick <lavashick@icloud.com>
2021-01-10 12:09:31 +0300

    первое

---
+++ app.js
@@ -0,0 +1,24 @@
+b1.onclick = function() {
+let a = 12;
+let b = "twelve";
+let c = !!true;
+let d = null;
+
+console.log(`a + b = ${a+b}`);
+//переменная а приводится к string
+
+console.log(`a + c = ${a+c}`);
+//переменная с приводится к num
+
+console.log(`a + d = ${a+d}`);
+//переменная d приводится к num, но становится равной 0
+
+console.log(`b + c = ${b+c}`);
+//переменная с приводится к string
+
+console.log(`b + d = ${b+d}`);
+//переменная d приводится к string
+
+console.log(`c + d = ${c+d}`);
+//переменные c d приводятся к num
+};
